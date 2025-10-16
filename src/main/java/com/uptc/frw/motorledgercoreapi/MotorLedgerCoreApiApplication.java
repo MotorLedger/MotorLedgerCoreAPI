908c74f32eef823af3319a0d46c7d2d37266c88f
Daniel Torres <daniel.torres.sw.dev@gmail.com>
2025-10-16 00:15:04 -0500

    initial version

---
+++ src/main/java/com/uptc/frw/motorledgercoreapi/MotorLedgerCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.motorledgercoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorLedgerCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorLedgerCoreApiApplication.class, args);
+	}
+
+}
